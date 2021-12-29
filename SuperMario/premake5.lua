e87bb67d93387aaa3fb557b6e63116bb099d91a1
DamianoPellegrini <damiano1.pellegrini@gmail.com>
2021-12-29 02:30:34 +0100

    feat: :add: added glad

--- SuperMario/premake5.lua
+++ SuperMario/premake5.lua
@@ -1,48 +1,49 @@
 project "SuperMario"
-kind "ConsoleApp"
-language "C++"
-cppdialect "C++20"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
 
-pchheader "pch.hpp"
-pchsource "%{prj.location}/src/pch.cpp"
+    pchheader "pch.hpp"
+    pchsource "%{prj.location}/src/pch.cpp"
 
-targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
-objdir "%{wks.location}/obj/%{cfg.buildcfg}"
+    targetdir "%{wks.location}/bin/%{cfg.buildcfg}"
+    objdir "%{wks.location}/obj/%{cfg.buildcfg}"
 
-files {"%{prj.location}/src/**.cpp", "%{prj.location}/src/**.hpp"}
+    files {"%{prj.location}/src/**.cpp", "%{prj.location}/src/**.hpp"}
 
-includedirs {"%{prj.location}/src"}
+    includedirs {"%{prj.location}/src"}
 
-sysincludedirs {
-    "%{wks.location}/dependencies/glfw/include",
-    "%{wks.location}/dependencies/spdlog/include",
-    "%{wks.location}/dependencies/json/include",
-    "%{wks.location}/dependencies/glm"
-}
+    sysincludedirs {
+        "%{wks.location}/dependencies/glfw/include",
+        "%{wks.location}/dependencies/glad/include",
+        "%{wks.location}/dependencies/spdlog/include",
+        "%{wks.location}/dependencies/json/include",
+        "%{wks.location}/dependencies/glm"
+    }
 
-links {"glfw"}
+    links {"glfw", "glad"}
 
-filter "system:windows"
+    filter "system:windows"
         targetname "%{prj.name}.exe"
-sysincludedirs {os.getenv("VULKAN_SDK") .. "/Include"}
-libdirs {os.getenv("VULKAN_SDK") .. "/Lib"}
-links {"vulkan-1"}
+        sysincludedirs {os.getenv("VULKAN_SDK") .. "/Include"}
+        libdirs {os.getenv("VULKAN_SDK") .. "/Lib"}
+        links {"vulkan-1"}
 
-filter "system:linux"
+    filter "system:linux"
         targetname "%{prj.name}"
-buildoptions {"-Wvolatile"}
-links {"vulkan", "dl", "pthread", "X11", "Xrandr", "Xxf86vm", "Xi"}
+        buildoptions {"-Wvolatile"}
+        links {"vulkan", "dl", "pthread", "X11", "Xrandr", "Xxf86vm", "Xi"}
 
-filter "system:macosx"
+    filter "system:macosx"
         targetname "%{prj.name}.app"
-links {
-    "Cocoa.framework", "IOKit.framework", "CoreVideo.framework",
-    "vulkan"
-}
-buildoptions {"-Wdeprecated-volatile", "-Wdeprecated-declarations"}
+        links {
+            "Cocoa.framework", "IOKit.framework", "CoreVideo.framework",
+            "vulkan"
+        }
+        buildoptions {"-Wdeprecated-volatile", "-Wdeprecated-declarations"}
 
--- Cross platform dependencies
-filter {}
-postbuildcommands {
-    "{COPY} %{wks.location}/run/** %{cfg.targetdir}"
-}
+    -- Cross platform dependencies
+    filter {}
+        postbuildcommands {
+            "{COPY} %{wks.location}/run/** %{cfg.targetdir}"
+        }
--- premake5.lua
+++ premake5.lua
@@ -1,36 +1,37 @@
 workspace "SuperMario"
-configurations {"Debug", "Profile", "Release"}
--- architecture "x86_64"
-flags {"MultiProcessorCompile"}
+    configurations {"Debug", "Profile", "Release"}
+    -- architecture "x86_64"
+    flags {"MultiProcessorCompile"}
 
-filter "configurations:Debug"
-defines {"DEBUG"}
-runtime "Debug"
-symbols "On"
+    filter "configurations:Debug"
+        defines {"DEBUG"}
+        runtime "Debug"
+        symbols "On"
 
-filter "configurations:Release"
-defines {"NDEBUG"}
-runtime "Release"
-optimize "On"
+    filter "configurations:Release"
+        defines {"NDEBUG"}
+        runtime "Release"
+        optimize "On"
 
-filter "system:windows"
-system "windows"
-vectorextensions "SSE2"
+    filter "system:windows"
+        system "windows"
+        vectorextensions "SSE2"
 
-filter "system:linux"
-system "linux"
-vectorextensions "SSE2"
-flags {"LinkTimeOptimization"}
+    filter "system:linux"
+        system "linux"
+        vectorextensions "SSE2"
+        flags {"LinkTimeOptimization"}
 
-filter "system:macosx"
-system "macosx"
+    filter "system:macosx"
+        system "macosx"
 
--- Deactivate filtering
-filter {}
+    -- Deactivate filtering
+    filter {}
 
--- Adding every dependency that need to be compiled
-group "Dependencies"
-include "dependencies/glfw.lua"
+    -- Adding every dependency that need to be compiled
+    group "Dependencies"
+        include "dependencies/glfw.lua"
+        include "dependencies/glad.lua"
 
-group ""
-include "SuperMario"
+    group ""
+        include "SuperMario"
