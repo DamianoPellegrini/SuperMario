80c8e56d57aa29fb7187c48937c0c5f72fdc6abc
DamianoPellegrini <damiano1.pellegrini@gmail.com>
2022-01-17 02:03:15 +0100

    refactor: :art: ported vulkan code to the cpp header

---
+++ SuperMario/src/vulkan_extensions.hpp
@@ -0,0 +1,9 @@
+#ifndef _VULKAN_EXTENSIONS_HPP
+#define _VULKAN_EXTENSIONS_HPP
+
+#include <vulkan/vulkan.hpp>
+
+VkResult vkCreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+void vkDestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+
+#endif // _VULKAN_EXTENSIONS_HPP
